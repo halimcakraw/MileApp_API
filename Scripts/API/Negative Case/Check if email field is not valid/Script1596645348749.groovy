4eb4c27721fe73b9cbb36393c7ab36cd857b3f7d
HALIM <HALIM@DESKTOP-FQI05PG>
2020-08-05 23:43:39 +0700

    update

--- Scripts/API/Negative Case/Check if email field is not valid/Script1596645348749.groovy
+++ Scripts/API/Negative Case/Check if email field is not valid/Script1596645348749.groovy
@@ -15,11 +15,11 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
-WS.comment('Phone number less than 8 digits')
+WS.comment('Email is empty')
 
 def organization_name = 'Organization 1'
 def full_name = 'Halim Cakra'
-def phone = '08991080'
+def phone = '08123456789'
 def email = 'halim_testingAPI@yahoo.com'
 def organizationname = 'organization name'
 def fullname = 'full name'
@@ -30,9 +30,9 @@ def email_address = 'email'
 responseBody = WS.sendRequest(findTestObject('API/MileApp', [('organizationname') : organizationname, ('fullname') : fullname, 
 			('phone_number') : phone_number, ('email_address') : email_address,
 			('organization_name') : organization_name, ('full_name') : full_name
-            , ('email') : email, ('phone') : phone]))
+            , ('email') : 'halimtesting@yahoo', ('phone') : phone]))
 
 WS.verifyResponseStatusCode(responseBody, 400)
 WS.verifyElementPropertyValue(responseBody, 'status', false)
-WS.verifyElementPropertyValue(responseBody, 'message', 'Phone number cannot less than 8 digits')
+WS.verifyElementPropertyValue(responseBody, 'message', 'Email is not valid.')
 
